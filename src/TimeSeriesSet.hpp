827afc413130a099875bccf5b591e0cffaf25116
Cuong Nguyen <nguyentricuong1995@gmail.com>
2017-03-19 00:48:55 -0400

    Fixed grammar error

--- src/TimeSeriesSet.hpp
+++ src/TimeSeriesSet.hpp
@@ -40,11 +40,11 @@ public:
   /**
    *  @brief loads data from a text file to this object
    *
-   *  Values in the text file have to be aranged in a table format. The number of
+   *  Values in the text file have to be arranged in a table format. The number of
    *  values (a.k.a columns) is inferred from the first line. All lines must have
    *  the same number of columns. If the number of lines exceeds maxNumRow, only
    *  maxNumRow lines are read and the rest is discarded. On the other hand, if
-   *  maxNumRow is larger than or equal to the actualy number of lines, all lines
+   *  maxNumRow is larger than or equal to the actual number of lines, all lines
    *  are read.
    *
    *  @param filePath path to a text file
