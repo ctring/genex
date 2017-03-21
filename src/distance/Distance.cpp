aeb3d4456f428a747cd9c88c0cc55d91a444e382
Cuong Nguyen <nguyentricuong1995@gmail.com>
2017-03-21 13:30:45 -0400

    Fixed minor style issues

--- src/distance/Distance.cpp
+++ src/distance/Distance.cpp
@@ -2,36 +2,36 @@
 
 namespace genex {
 
-  /**
-   *  @brief returns the warped distance between two sets of data
-   *
-   *  @param metric the distance metric to use
-   *  @param x_1 one of the two arrays of data
-   *  @param x_2 the other of the two arrays of data
-   *  @param x_3 the length of the data
-   */
-  data_t generalWarpedDistance(DistanceMetric* metric,TimeSeries x_1, TimeSeries x_2, int len)
-  {
-    //TODO implement this!!
-    //TODO add dropout
-    return generalDistance(metric, x_1, x_2, len);
-  }
-
-  /**
-   *  @brief returns the distance between two sets of data
-   *
-   *  @param metric the distance metric to use
-   *  @param x_1 one of the two arrays of data
-   *  @param x_2 the other of the two arrays of data
-   *  @param x_3 the length of the data
-   */
-  data_t generalDistance(DistanceMetric* metric, TimeSeries x_1, TimeSeries x_2, int len)
-  {
-    data_t total = 0.0;
-    for(int i = 0; i < len; i++) {
-      total = metric->reduce(total, x_1[i], x_2[i]);
-    }
-
-    return metric->norm(total, x_1, x_2);
-  }
+/**
+ *  @brief returns the warped distance between two sets of data
+ *
+ *  @param metric the distance metric to use
+ *  @param x_1 one of the two arrays of data
+ *  @param x_2 the other of the two arrays of data
+ *  @param x_3 the length of the data
+ */
+data_t generalWarpedDistance(DistanceMetric* metric,TimeSeries x_1, TimeSeries x_2, int len)
+{
+  //TODO implement this!!
+  //TODO add dropout
+  return generalDistance(metric, x_1, x_2, len);
 }
+
+/**
+ *  @brief returns the distance between two sets of data
+ *
+ *  @param metric the distance metric to use
+ *  @param x_1 one of the two arrays of data
+ *  @param x_2 the other of the two arrays of data
+ *  @param x_3 the length of the data
+ */
+data_t generalDistance(DistanceMetric* metric, TimeSeries x_1, TimeSeries x_2, int len)
+{
+  data_t total = 0.0;
+  for(int i = 0; i < len; i++) {
+    total = metric->reduce(total, x_1[i], x_2[i]);
+  }
+
+  return metric->norm(total, x_1, x_2);
+}
+} // namespace genex
--- src/distance/Distance.hpp
+++ src/distance/Distance.hpp
@@ -5,24 +5,26 @@
 #include "distance/DistanceMetric.hpp"
 
 namespace genex {
-  /**
-   *  @brief returns the warped distance between two sets of data
-   *
-   *  @param metric the distance metric to use
-   *  @param x_1 one of the time series
-   *  @param x_2 the other of the time series
-   *  @param x_3 the length of the data
-   */
-  data_t generalWarpedDistance(DistanceMetric* metric, TimeSeries x_1, TimeSeries x_2, int len);
+/**
+ *  @brief returns the warped distance between two sets of data
+ *
+ *  @param metric the distance metric to use
+ *  @param x_1 one of the time series
+ *  @param x_2 the other of the time series
+ *  @param x_3 the length of the data
+ */
+data_t generalWarpedDistance(DistanceMetric* metric, TimeSeries x_1, TimeSeries x_2, int len);
 
-  /**
-   *  @brief returns the distance between two sets of data
-   *
-   *  @param metric the distance metric to use
-   *  @param x_1 one of the time series
-   *  @param x_2 the other of the time series
-   *  @param x_3 the length of the data
-   */
-  data_t generalDistance(DistanceMetric* metric, TimeSeries x_1, TimeSeries x_2, int len);
-}
+/**
+ *  @brief returns the distance between two sets of data
+ *
+ *  @param metric the distance metric to use
+ *  @param x_1 one of the time series
+ *  @param x_2 the other of the time series
+ *  @param x_3 the length of the data
+ */
+data_t generalDistance(DistanceMetric* metric, TimeSeries x_1, TimeSeries x_2, int len);
+
+} // namespace genex
+
 #endif //GENERAL_DISTANCE_H
