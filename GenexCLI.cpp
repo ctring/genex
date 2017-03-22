135ff6346306f5f07609352e0b970b26b1b3df56
Cuong Nguyen <nguyentricuong1995@gmail.com>
2017-03-22 17:06:04 -0400

    Added Readline dependency

--- GenexCLI.cpp
+++ GenexCLI.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
+#include <readline/readline.h>
+#include <readline/history.h>
+
 #include "config.hpp"
-#include "distance/DistanceManager.cpp"
 
 int main (int argc, char *argv[])
 {
-  using namespace genex;
-
-#ifdef SINGLE_PRECISION
-  std::cout << "Single precision!\n";
-#endif
-
-  std::cout << "Unimplemented!\n";
-
-  const DistanceManager* m = DistanceManager::getInstance();
-  return 1;
+  char* line = readline("> ");
+  std::cout << std::string(line) << std::endl;
+  return 0;
 }
