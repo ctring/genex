b5c487e42a8b269f409290f4388aa2474d941cff
Cuong Nguyen <ctring23@gmail.com>
2017-10-09 21:42:50 -0400

    script for generating ksim experiments

--- scripts/gen_ksim_exp.py
+++ scripts/gen_ksim_exp.py
@@ -24,7 +24,11 @@ if __name__ == '__main__':
                                                  'dimension of a dataset.')
     parser.add_argument('count', type=int, help='number of items in the dataset.')
     parser.add_argument('length', type=int, help='length of each item in the dataset.')
-    parser.add_argument('-n', type=int, default=10,
+    parser.add_argument('exp_path', help='path to the GENEX experiment script')
+    parser.add_argument('ds_path', help='path to the dataset used in the experiment')
+    parser.add_argument('st', type=float, help='similarity threshold for the experiment')
+    parser.add_argument('k', help='number of similar time series to look for')
+    parser.add_argument('--n', type=int, default=10,
                         help='number of sequences to be picked (default: 10).')
     parser.add_argument('--min-length', type=int, default=2,
                         help='minimum length of each sequence (default: 2).')
@@ -36,3 +40,13 @@ if __name__ == '__main__':
     seq = pick_random(args.count, args.length, args.min_length, args.n)
     for s in seq:
         print(args.fmt.format(s[0], s[1], s[2]))
+    print()
+
+    with open(args.exp_path, 'w') as f:
+        print('load {}'.format(args.ds_path), file=f)
+        print('group 0 {}'.format(args.st), file=f)
+        for s in seq:
+            print('kSim {} 0 0 {} {} {} 0'.format(args.k, s[0], s[1], s[2]), 
+                  file=f)
+    print('Experiment script is generated at {}'.format(args.exp_path))
+    
