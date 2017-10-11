47a32fdc8912338b2f916d5bb5702870883bbb72
Cuong Nguyen <ctring23@gmail.com>
2017-10-10 20:39:16 -0400

    update experiment generator with new parameter

--- scripts/gen_ksim_exp.py
+++ scripts/gen_ksim_exp.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
+'''
+Example:
+    ./gen_ksim_exp.py 10 10 experiment.txt dataset.txt 0.3 -k 1 3 5 7 -m 1 2 3 4 5 6 7 --seed 23 -n 10
+'''
 
 from __future__ import absolute_import
 from __future__ import division
@@ -20,17 +24,19 @@ def pick_random(count, length, min_length=2, n=10):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Picks random time series given the'
+    parser = argparse.ArgumentParser(description='Picks random time series given the '
                                                  'dimension of a dataset.')
     parser.add_argument('count', type=int, help='number of items in the dataset.')
     parser.add_argument('length', type=int, help='length of each item in the dataset.')
     parser.add_argument('exp_path', help='path to the GENEX experiment script')
     parser.add_argument('ds_path', help='path to the dataset used in the experiment')
     parser.add_argument('st', type=float, help='similarity threshold for the experiment')
-    parser.add_argument('k', nargs='+', 
+    parser.add_argument('-k', nargs='+', 
                         help='number of similar time series to look for.'
                              'Multiple values can be specified (separated by space).')
-    parser.add_argument('--n', type=int, default=10,
+    parser.add_argument('-m', nargs='+',
+                        help='m*k = number of similar time series to examine.')
+    parser.add_argument('-n', type=int, default=10,
                         help='number of sequences to be picked (default: 10).')
     parser.add_argument('--seed', type=int,
                         help='seed for the random number generator.')
@@ -52,8 +58,8 @@ if __name__ == '__main__':
         print('load {}'.format(args.ds_path), file=f)
         print('group 0 {}'.format(args.st), file=f)
         for k in args.k:
-            for s in seq:
-                print('testSim {} 0 0 {} {} {}'.format(k, s[0], s[1], s[2]), 
-                    file=f)
+            for m in args.m:
+                for s in seq:
+                    print('testSim {} {} 0 0 {} {} {}'.format(k, int(k)*int(m), s[0], s[1], s[2]), 
+                          file=f)
     print('Experiment script is generated at {}'.format(args.exp_path))
-    
