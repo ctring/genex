f3872392bfceebdd6d4f82592b0323ae4631e24a
Cuong Nguyen <ctring23@gmail.com>
2018-02-21 18:04:17 -0500

    Handy script for building genex

---
+++ build.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+show_help() {
+	echo "usage: $0 [run|test] [-r] [-d] [-h]"
+	echo "Build GENEX if no argument is provided. Otherwise:"
+	echo "	run     build then run"
+	echo "	test    build then test"
+	echo "Options:"
+	echo "  -d		compile in debug mode"
+	echo "  -r		rebuild everything"
+	echo "  -h		show this help"
+}
+
+OPTS=$(getopt -o rdh  -n "$0" -- "$@")
+
+if [ $? != 0 ] ; then show_help; exit 1; fi
+
+REBUILD=false
+DEBUG=false
+SHOW_HELP=false
+
+while true; do
+	case "$1" in
+		-r ) 
+			REBUILD=true; shift; ;;
+		-d )
+			DEBUG=true; shift; ;;
+		-h )
+			SHOW_HELP=true; shift; ;;
+		-- ) 
+			shift; break; ;;
+		* ) 
+			break ;;
+	esac
+done
+
+if [ $SHOW_HELP = true ]; then
+	show_help
+	exit 0
+fi
+
+if [ $# -gt 1 ]; then
+	echo "$0: too many arguments"
+	show_help
+	exit 1
+fi
+
+if [ $# -eq 1 ] && [ "$1" != "run" ] && [ "$1" != "test" ]; then
+	echo "$0: unknown argument - $1"
+	show_help
+	exit 1
+fi
+
+echo "Create and enter the 'build' directory"
+mkdir -p build 
+cd build
+
+if [ $DEBUG ]; then
+	echo "Build in Debug mode"
+	CMAKE_FLAGS="${CMAKE_FLAGS} -DCMAKE_BUILD_TYPE=Debug"
+fi
+
+cmake .. $CMAKE_FLAGS
+CPU=$(lscpu | grep ^CPU\(s\): | tr -s " " | cut -f2 -d " ")
+BUILD_CPU=$(expr $CPU / 2)
+
+if [ $REBUILD = true ]; then
+	make clean
+fi
+
+echo "Number of CPU(s): $CPU"
+echo "Building with $BUILD_CPU CPU(s)"
+make -j $BUILD_CPU
+
+cp -v pygenex.so ../scripts/ 
+
+if [ "$1" = "run" ]; then
+	./genexcli
+elif [ "$1" = "test" ]; then
+	ctest -V
+fi
+
+exit 0
